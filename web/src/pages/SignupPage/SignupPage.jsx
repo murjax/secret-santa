979b1e1d3d2f8e7bc1f33be3bced213c63aeef67
Ryan Murphy <murjax@gmail.com>
2023-12-03 13:56:38 -0500

    Day 3: Styling the Login Pages

--- web/src/pages/SignupPage/SignupPage.jsx
+++ web/src/pages/SignupPage/SignupPage.jsx
@@ -14,6 +14,7 @@ import { MetaTags } from '@redwoodjs/web'
 import { toast, Toaster } from '@redwoodjs/web/toast'
 
 import { useAuth } from 'src/auth'
+import AuthLayout from 'src/layouts/AuthLayout/AuthLayout'
 
 const SignupPage = () => {
   const { isAuthenticated, signUp } = useAuth()
@@ -50,78 +51,57 @@ const SignupPage = () => {
     <>
       <MetaTags title="Signup" />
 
-      <main className="rw-main">
+      <AuthLayout>
         <Toaster toastOptions={{ className: 'rw-toast', duration: 6000 }} />
-        <div className="rw-scaffold rw-login-container">
-          <div className="rw-segment">
-            <header className="rw-segment-header">
-              <h2 className="rw-heading rw-heading-secondary">Signup</h2>
-            </header>
+        <div className="rw-login-container w-2/5">
+          <div className="rw-form-wrapper">
+            <Form onSubmit={onSubmit}>
+              <TextField
+                placeholder="EMAIL"
+                name="email"
+                className="rw-input p-4 placeholder-black font-handwriting text-2xl"
+                errorClassName="rw-input rw-input-error"
+                ref={emailRef}
+                validation={{
+                  required: {
+                    value: true,
+                    message: 'Email is required',
+                  },
+                }}
+              />
 
-            <div className="rw-segment-main">
-              <div className="rw-form-wrapper">
-                <Form onSubmit={onSubmit} className="rw-form-wrapper">
-                  <Label
-                    name="email"
-                    className="rw-label"
-                    errorClassName="rw-label rw-label-error"
-                  >
-                    Email
-                  </Label>
-                  <TextField
-                    name="email"
-                    className="rw-input"
-                    errorClassName="rw-input rw-input-error"
-                    ref={emailRef}
-                    validation={{
-                      required: {
-                        value: true,
-                        message: 'Email is required',
-                      },
-                    }}
-                  />
+              <FieldError name="email" className="rw-field-error" />
 
-                  <FieldError name="email" className="rw-field-error" />
+              <PasswordField
+                placeholder="PASSWORD"
+                name="password"
+                className="rw-input p-4 placeholder-black font-handwriting text-2xl"
+                errorClassName="rw-input rw-input-error"
+                autoComplete="current-password"
+                validation={{
+                  required: {
+                    value: true,
+                    message: 'Password is required',
+                  },
+                }}
+              />
 
-                  <Label
-                    name="password"
-                    className="rw-label"
-                    errorClassName="rw-label rw-label-error"
-                  >
-                    Password
-                  </Label>
-                  <PasswordField
-                    name="password"
-                    className="rw-input"
-                    errorClassName="rw-input rw-input-error"
-                    autoComplete="current-password"
-                    validation={{
-                      required: {
-                        value: true,
-                        message: 'Password is required',
-                      },
-                    }}
-                  />
+              <FieldError name="password" className="rw-field-error" />
 
-                  <FieldError name="password" className="rw-field-error" />
-
-                  <div className="rw-button-group">
-                    <Submit className="rw-button rw-button-blue">
-                      Sign Up
-                    </Submit>
-                  </div>
-                </Form>
+              <div className="rw-button-group">
+                <Submit className="bg-supernova w-full rounded-full text-2xl font-handwriting font-bold py-4">
+                  SUBMIT
+                </Submit>
               </div>
-            </div>
+            </Form>
           </div>
           <div className="rw-login-link">
-            <span>Already have an account?</span>{' '}
-            <Link to={routes.login()} className="rw-link">
-              Log in!
+            <Link to={routes.login()} className="rw-link text-black">
+              Ready to Login?
             </Link>
           </div>
         </div>
-      </main>
+      </AuthLayout>
     </>
   )
 }
